b0b37dee2cbc1f6d086493923c4da75e8cde412d
James Noss <jnoss@jhu.edu>
2023-06-23 14:19:46 -0400

    Add __str__() funcs, validators, docstrings, and some other stuff
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/uploader/models.py
+++ biospecdb/apps/uploader/models.py
@@ -1,8 +1,11 @@
 from enum import auto
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Changes here need to be migrated, committed, and activated.
 # See https://docs.djangoproject.com/en/4.2/intro/tutorial02/#activating-models
@@ -15,6 +18,36 @@ from django.db import models
 POSITIVE = "positive"
 NEGATIVE = "negative"
 
+# https://www.hhs.gov/hipaa/for-professionals/privacy/special-topics/de-identification/index.html
+# NOTE: Examples of PHI:
+# Medical record number.
+# Health plan beneficiary number.
+# Device identifiers or serial numbers.
+# An MRI scan.
+# Blood test results.
+# Any other unique identifying numbers, characteristics, or codes.
+
+# The following PII is considered PHI when accompanied by health data (i.e., as it would be for this project).
+# Name.
+# Address (including subdivisions smaller than state such as street address, city, county, or zip code).
+# All elements of dates (except year) for dates that are directly related to an individual, including birth date,
+# admission date, discharge date, death date, and all ages over 89 and all elements of dates (including year)
+# indicative of such age, except that such ages and elements may be aggregated into a single category of age 90 or
+# older.
+# Telephone number.
+# Fax number.
+# Email address.
+# Social Security number.
+# Social Security number.
+# Account number.
+# Certificate/license number.
+# Vehicle identifiers, serial numbers, or license plate numbers.
+# Web URLs.
+# IP address.
+# Biometric identifiers such as fingerprints or voice prints.
+# Full-face photos.
+# Any other unique identifying numbers, characteristics, or codes.
+
 
 class UploadedFile(models.Model):
     file = models.FileField(upload_to='./biospecdb/apps/uploader/uploads/')
@@ -22,8 +55,9 @@ class UploadedFile(models.Model):
 
 
 class Patient(models.Model):
+    """ Model an individual patient. """
     MIN_AGE = 0
-    MAX_AGE = 150
+    MAX_AGE = 90  # Required by HIPAA.
 
     class Gender(models.TextChoices):
         MALE = auto()
@@ -32,31 +66,81 @@ class Patient(models.Model):
     patient_id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
     gender = models.CharField(max_length=8, choices=Gender.choices, null=True)
 
+    def __str__(self):
+        return str(self.patient_id)
+
+    def short_id(self):
+        return str(self.patient_id)[:8]
+
 
 class Visit(models.Model):
+    """ Model a patient's visitation to collect health data and biological samples.  """
+
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name="visit")
-    previous_visit = models.ForeignKey("self", null=True, on_delete=models.SET_NULL, related_name="next_visit")
+
+    # NOTE: This has to allow for blank to accommodate the initial vist for which there is no prior.
+    previous_visit = models.ForeignKey("self", default=None, blank=True, null=True, on_delete=models.SET_NULL,
+                                       related_name="next_visit")
 
     patient_age = models.IntegerField(validators=[MinValueValidator(Patient.MIN_AGE),
                                                   MaxValueValidator(Patient.MAX_AGE)])
 
+    def clean(self):
+        """ Model validation. """
+        if hasattr(super, "clean"):
+            super.clean()
+
+        # Validate visits belong to same patient.
+        if self.previous_visit is not None and (self.previous_visit.patient_id != self.patient_id):
+            raise ValidationError(_("Previous visits do not belong to this patient!"), code="invalid")
+
+        # Validate visits are entered ordered by age.
+        if self.previous_visit is not None and (self.patient_age < self.previous_visit.patient_age):
+            raise ValidationError(_("Previous visit must NOT be older than this one: patient age before %(prior_age)i "
+                                    " > %(current_age)i"),
+                                  params={"current_age": self.patient_age, "prior_age": self.previous_visit.patient_age},
+                                  code="invalid")
+
+    def count_prior_visits(self):
+        return 0 if self.previous_visit is None else 1 + self.previous_visit.count_prior_visits()
+
+    @property
+    def visit_number(self):
+        return 1 + self.count_prior_visits()
+
+    def __str__(self):
+        return f"patient:{self.patient.short_id()}_visit:{self.visit_number}"
+
 
 class Disease(models.Model):
+    """ Model an individual disease, symptom, or health condition. A patient's instance are stored as models.Symptom"""
+
+    class Types(models.TextChoices):
+        BOOL = auto()
+        STR = auto()
+        INT = auto()
+        FLOAT = auto()
+
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
 
     # This represents ths type/class for Symptom.disease_value.
-    value_class = models.CharField(max_length=128, blank=True, null=True)
+    # NOTE: For bool types, Symptom.is_symptomatic may suffice.
+    value_class = models.CharField(max_length=128, default=None, blank=True, null=True, choices=Types.choices)
+
+    def __str__(self):
+        return self.name
 
 
 class Symptom(models.Model):
+    """ A patient's instance of models.Disease. """
     MIN_SEVERITY = 0
     MAX_SEVERITY = 10
 
     visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name="symptom")
     disease = models.ForeignKey(Disease, on_delete=models.CASCADE, related_name="symptom")
 
-    was_asked = models.BooleanField(default=True)
+    was_asked = models.BooleanField(default=True)  # Whether the patient was asked whether they have this symptom.
     is_symptomatic = models.BooleanField(default=True)
     days_symptomatic = models.IntegerField(default=0, blank=True, null=True,
                                            validators=[MinValueValidator(0)])  # TODO: max <= age
@@ -67,8 +151,13 @@ class Symptom(models.Model):
     # Str format for actual type/class spec'd by Disease.value_class.
     disease_value = models.CharField(blank=True, null=True, max_length=128)
 
+    def __str__(self):
+        return f"patient:{self.visit.patient.short_id()}_{self.disease.name}"
+
 
 class Instrument(models.Model):
+    """ Model the instrument/device used to measure spectral data (not the collection of the bio sample). """
+
     class Spectrometers(models.TextChoices):
         AGILENT_COREY_630 = auto()
 
@@ -80,8 +169,12 @@ class Instrument(models.Model):
     atr_crystal = models.CharField(default=SpectrometerCrystal.ZNSE, max_length=128,
                                    choices=SpectrometerCrystal.choices)
 
+    def __str__(self):
+        return self.spectrometer
+
 
 class BioSample(models.Model):
+    """ Model biological sample and collection method. """
     class SampleKind(models.TextChoices):
         PHARYNGEAL_SWAB = auto()
 
@@ -93,8 +186,12 @@ class BioSample(models.Model):
     freezing_time = models.IntegerField(blank=True, null=True)
     thawing_time = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.visit}_type:{self.sample_type}_pk{self.pk}"  # NOTE: str(self.visit) contains patient ID.
+
 
 class SpectralData(models.Model):
+    """ Model spectral data measured by spectrometer instrument. """
     class SpectralMeasurementKind(models.TextChoices):
         ATR_FTIR = auto()
 
@@ -114,6 +211,9 @@ class SpectralData(models.Model):
     # TODO: We could write a custom storage class to write these all to a parquet table instead of individual files.
     # See https://docs.djangoproject.com/en/4.2/howto/custom-file-storage/
     data = models.FileField(upload_to="uploads/")
+
+    def __str__(self):
+        return f"{self.bio_sample.visit}_pk{self.pk}"
 
 
 # This is Model B wo/ disease table https://miro.com/app/board/uXjVMAAlj9Y=/
