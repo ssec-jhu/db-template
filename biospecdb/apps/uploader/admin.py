1c2513b72625194617f21da51b89eee1ca51d60f
James Noss <jnoss@jhu.edu>
2023-06-23 16:41:46 -0400

    Exclude BioSample.qc_metrics from admin form
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/uploader/admin.py
+++ biospecdb/apps/uploader/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import BioSample, Disease, Instrument, Patient, SpectralData, Symptom, UploadedFile, Visit
 
+
+class SpectralDataAdmin(admin.ModelAdmin):
+    exclude = ["qc_metrics"]
+
+
 admin.site.register(BioSample)
 admin.site.register(Disease)
 admin.site.register(Instrument)
 admin.site.register(Patient)
-admin.site.register(SpectralData)
+admin.site.register(SpectralData, SpectralDataAdmin)
 admin.site.register(Symptom)
 admin.site.register(Visit)
 
