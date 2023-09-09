a3c35fe5c7dda7f0e4c3a8604886c2de6b15d01b
Vladimir Kluzner <vkluzner@jhu.edu>
2023-09-08 18:32:35 -0700

    Add 'required' and 'initial' for every form field

--- biospecdb/apps/uploader/forms.py
+++ biospecdb/apps/uploader/forms.py
@@ -12,46 +12,74 @@ class FileUploadForm(forms.ModelForm):
 
 class DataInputForm(forms.Form):
         
-    patient_id = forms.IntegerField(initial=False, required=True, label="Patient ID")
+    patient_id = forms.IntegerField(required=True,
+                                    initial=False,
+                                    label="Patient ID")
     gender = forms.ChoiceField(required=not Patient.gender.field.blank,
-                               choices=Patient.Gender.choices,
-                               label=Patient.gender.field.verbose_name)
+                               initial=Patient.gender.field.default,
+                               label=Patient.gender.field.verbose_name,
+                               choices=Patient.Gender.choices)
     days_symptomatic = forms.IntegerField(required=not Symptom.days_symptomatic.field.blank,
+                                          initial=Symptom.days_symptomatic.field.default,
                                           label=Symptom.days_symptomatic.field.verbose_name)
-    patient_age = forms.IntegerField(label=Visit.patient_age.field.verbose_name)
-    spectra_measurement = forms.ChoiceField(choices=SpectralData.SpectralMeasurementKind.choices,
-                                            label=SpectralData.spectra_measurement.field.verbose_name)
-    spectrometer = forms.ChoiceField(choices=Instrument.Spectrometers.choices,
-                                     label=Instrument.spectrometer.field.verbose_name)
-    atr_crystal = forms.ChoiceField(choices=Instrument.SpectrometerCrystal.choices,
-                                    label=Instrument.atr_crystal.field.verbose_name)
+    patient_age = forms.IntegerField(required=not Visit.patient_age.field.blank,
+                                     initial=Visit.patient_age.field.default,
+                                     label=Visit.patient_age.field.verbose_name)
+    spectra_measurement = forms.ChoiceField(required=not SpectralData.spectra_measurement.field.blank,
+                                            initial=SpectralData.spectra_measurement.field.default,
+                                            label=SpectralData.spectra_measurement.field.verbose_name,
+                                            choices=SpectralData.SpectralMeasurementKind.choices)
+    spectrometer = forms.ChoiceField(required=not Instrument.spectrometer.field.blank,
+                                     initial=Instrument.spectrometer.field.default,
+                                     label=Instrument.spectrometer.field.verbose_name,
+                                     choices=Instrument.Spectrometers.choices)
+    atr_crystal = forms.ChoiceField(required=not Instrument.atr_crystal.field.blank,
+                                    initial=Instrument.atr_crystal.field.default,
+                                    label=Instrument.atr_crystal.field.verbose_name,
+                                    choices=Instrument.SpectrometerCrystal.choices)
     acquisition_time = forms.IntegerField(required=not SpectralData.acquisition_time.field.blank,
+                                          initial=SpectralData.acquisition_time.field.default,
                                           label=SpectralData.acquisition_time.field.verbose_name)
-    n_coadditions = forms.IntegerField(initial=SpectralData.n_coadditions.field.default,
+    n_coadditions = forms.IntegerField(required=not SpectralData.n_coadditions.field.blank,
+                                       initial=SpectralData.n_coadditions.field.default,
                                        label=SpectralData.n_coadditions.field.verbose_name)
     resolution = forms.IntegerField(required=not SpectralData.resolution.field.blank,
+                                    initial=SpectralData.resolution.field.default,
                                     label=SpectralData.resolution.field.verbose_name)
-    sample_type = forms.ChoiceField(choices=BioSample.SampleKind.choices,
-                                    label=BioSample.sample_type.field.verbose_name)
-    sample_processing = forms.CharField(initial=BioSample.sample_processing.field.default,
-                                        required=not BioSample.sample_processing.field.blank,
+    sample_type = forms.ChoiceField(required=not BioSample.sample_type.field.blank,
+                                    initial=BioSample.sample_type.field.default,
+                                    label=BioSample.sample_type.field.verbose_name,
+                                    choices=BioSample.SampleKind.choices)
+    sample_processing = forms.CharField(required=not BioSample.sample_processing.field.blank,
+                                        initial=BioSample.sample_processing.field.default,
                                         label=BioSample.sample_processing.field.verbose_name)
     freezing_temp = forms.FloatField(required=not BioSample.freezing_temp.field.blank,
+                                     initial=BioSample.freezing_temp.field.default,
                                      label=BioSample.freezing_temp.field.verbose_name)
     thawing_time = forms.IntegerField(required=not BioSample.thawing_time.field.blank,
+                                      initial=BioSample.thawing_time.field.default,
                                       label=BioSample.thawing_time.field.verbose_name)
     spectral_data = forms.FileField(label="Spectral data file")
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for disease in Disease.objects.all(): # Dynamically add disease fields from the Disease table
-            if (field_type := disease.value_class) == "BOOL":
-                field_type = forms.BooleanField(required=False, label=disease.alias, widget=forms.NullBooleanSelect)
-                #field_type = forms.BooleanField(initial=False, required=False, label=disease.alias)
-            elif field_type == "FLOAT":
-                field_type = forms.FloatField(initial=False, required=False, label=disease.alias)
+            if (field_type := disease.value_class) == "FLOAT":
+                field_type = forms.FloatField(required=False,
+                                              initial=False,
+                                              label=disease.alias)
             elif field_type == "STR":
-                field_type = forms.CharField(required=False, label=disease.alias)
+                field_type = forms.CharField(required=False,
+                                             initial='',
+                                             label=disease.alias)
+            elif field_type == "BOOL":
+                field_type = forms.BooleanField(required=False,
+                                                initial=False,
+                                                label=disease.alias)
+                #To be reconsidered later
+                #field_type = forms.BooleanField(required=False,
+                #                                label=disease.alias,
+                #                                widget=forms.NullBooleanSelect)
             self.fields[disease.name] = field_type
 
     def populate_meta_data(self):
