bed889358f0e8748b0b60520624baae2d38ab7ab
James Noss <jnoss@jhu.edu>
2023-09-26 16:29:28 -0400

    Add admin
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/user/admin.py
+++ biospecdb/apps/user/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from user.models import Center, User
 
-admin.site.register(User, BaseUserAdmin)
 admin.site.register(Center)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_form_template = "admin/auth/user/add_form.html"
+    change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "center")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "center"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "center", "first_name", "last_name", "is_staff")
+    list_filter = ("center", "is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "center", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
