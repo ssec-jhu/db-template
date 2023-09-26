4246004c529102c7384258a222a00704aa190d63
James Noss <jnoss@jhu.edu>
2023-09-26 16:10:44 -0400

    Add center admin
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/user/admin.py
+++ biospecdb/apps/user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from user.models import User
+from user.models import Center, User
 
 admin.site.register(User, BaseUserAdmin)
+admin.site.register(Center)
