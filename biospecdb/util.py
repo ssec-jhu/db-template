b045d3c1decc4bbfbeb70846712c17d4734a49bb
James Noss <jnoss@jhu.edu>
2023-09-28 11:49:09 -0400

    Update base utility tests
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/util.py
+++ biospecdb/util.py
@@ -11,11 +11,11 @@ from . import __project__  # Keep as relative for templating reasons.
 
 
 def find_package_location(package=__project__):
-    return importlib.util.find_spec(package).submodule_search_locations[0]
+    return Path(importlib.util.find_spec(package).submodule_search_locations[0])
 
 
 def find_repo_location(package=__project__):
-    return os.path.abspath(os.path.join(find_package_location(package), os.pardir))
+    return Path(find_package_location(package) / os.pardir)
 
 
 class StrEnum(enum.StrEnum):
