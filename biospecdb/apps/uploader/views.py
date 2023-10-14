de18a0224a8f47cece98ab52ec8e43a0f7307f55
Vladimir Kluzner <vkluzner@jhu.edu>
2023-10-14 14:52:23 -0700

    Add database lock functionality

--- biospecdb/apps/uploader/views.py
+++ biospecdb/apps/uploader/views.py
@@ -1,6 +1,8 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+from django.db import transaction
 from openpyxl import load_workbook
+
 from .forms import FileUploadForm, DataInputForm
 from uploader.models import Patient, Visit, SpectralData, BioSample, Symptom, Disease
 from biospecdb.util import is_valid_uuid, to_uuid
@@ -56,67 +58,73 @@ def data_input(request):
                     'delta_count': delta_count})
             else:
                 patient_id = to_uuid(patient_id)
-                try:
-                    patient = Patient.objects.get(patient_id=patient_id)
-                except (Patient.DoesNotExist):
-                    message = "Data Search failed - there is no data associated with Patient ID {}.".format(patient_id)
+                with transaction.atomic():
+                    try:
+                        patient = Patient.objects.select_for_update().get(patient_id=patient_id)
+                    except (Patient.DoesNotExist):
+                        message = "Data Search failed - there is no data associated with Patient ID {}.".format(patient_id)
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
+                            'delta_count': delta_count})
+                    try:
+                        last_visit = Visit.objects.select_for_update().filter(patient_id=patient_id).order_by('created_at').last()
+                    except (Visit.DoesNotExist):
+                        message = "Data Search failed - there is no any visit of patient with Patient ID {}." \
+                            .format(patient_id)
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
+                            'delta_count': delta_count})
+                    try:
+                        biosample = BioSample.objects.select_for_update().get(visit=last_visit)
+                    except (BioSample.DoesNotExist):
+                        message = "Data Search failed - there is no biosample associated with the visit {}." \
+                            .format(last_visit)
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
+                            'delta_count': delta_count})
+                    try:
+                        last_visit_symptoms = Symptom.objects.select_for_update().filter(visit=last_visit)
+                        symptom = last_visit_symptoms.order_by('days_symptomatic').last()
+                    except (Symptom.DoesNotExist):
+                        message = "Data Search failed - there are no symptoms associated with the visit {}." \
+                            .format(last_visit)
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
+                            'delta_count': delta_count})
+                    try:
+                        spectraldata = SpectralData.objects.select_for_update().get(bio_sample=biosample)
+                    except (SpectralData.DoesNotExist):
+                        message = "Data Search failed - there is no spectral data associated with the biosample {}." \
+                            .format(biosample)
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
+                            'delta_count': delta_count})
+                        
+                    except Exception as e: # Handle other exceptions as needed.
+                        message = "An error occurred during data retrieval: {}".format(str(e))
+                        return render(request, 'DataInputForm.html', {'form': form, 'message': message, 'delta_count': delta_count})
+                
+                    initial_data={
+                        'patient_id': patient_id,
+                        'gender': patient.gender,
+                        'days_symptomatic': symptom.days_symptomatic,
+                        'patient_age': last_visit.patient_age,
+                        'spectra_measurement': spectraldata.spectra_measurement,
+                        'instrument': spectraldata.instrument,
+                        'acquisition_time': spectraldata.acquisition_time,
+                        'n_coadditions': spectraldata.n_coadditions,
+                        'resolution': spectraldata.resolution,
+                        'sample_type': biosample.sample_type,
+                        'sample_processing': biosample.sample_processing,
+                        'freezing_temp': biosample.freezing_temp,
+                        'thawing_time': biosample.thawing_time,
+                        'spectral_data': spectraldata.data
+                    }
+                    for symptom in last_visit_symptoms:
+                        if symptom.disease.value_class == "BOOL":
+                            initial_data[symptom.disease.name] = \
+                                Disease.Types(symptom.disease.value_class).cast(symptom.disease_value)   
+                        else:
+                            initial_data[symptom.disease.name] = symptom.disease_value
+                    form = DataInputForm(initial=initial_data)
+                    message = "The data associated with Patient ID {} is shown below:".format(patient_id)
                     return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
                         'delta_count': delta_count})
-                try:
-                    last_visit = Visit.objects.filter(patient_id=patient_id).order_by('created_at').last()
-                except (Visit.DoesNotExist):
-                    message = "Data Search failed - there is no any visit of patient with Patient ID {}." \
-                        .format(patient_id)
-                    return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
-                        'delta_count': delta_count})
-                try:
-                    biosample = BioSample.objects.get(visit=last_visit)
-                except (BioSample.DoesNotExist):
-                    message = "Data Search failed - there is no biosample associated with the visit {}." \
-                        .format(last_visit)
-                    return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
-                        'delta_count': delta_count})
-                try:
-                    last_visit_symptoms = Symptom.objects.filter(visit=last_visit)
-                    symptom = last_visit_symptoms.order_by('days_symptomatic').last()
-                except (Symptom.DoesNotExist):
-                    message = "Data Search failed - there are no symptoms associated with the visit {}." \
-                        .format(last_visit)
-                    return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
-                        'delta_count': delta_count})
-                try:
-                    spectraldata = SpectralData.objects.get(bio_sample=biosample)
-                except (SpectralData.DoesNotExist):
-                    message = "Data Search failed - there is no spectral data associated with the biosample {}." \
-                        .format(biosample)
-                    return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
-                        'delta_count': delta_count})
-                initial_data={
-                    'patient_id': patient_id,
-                    'gender': patient.gender,
-                    'days_symptomatic': symptom.days_symptomatic,
-                    'patient_age': last_visit.patient_age,
-                    'spectra_measurement': spectraldata.spectra_measurement,
-                    'instrument': spectraldata.instrument,
-                    'acquisition_time': spectraldata.acquisition_time,
-                    'n_coadditions': spectraldata.n_coadditions,
-                    'resolution': spectraldata.resolution,
-                    'sample_type': biosample.sample_type,
-                    'sample_processing': biosample.sample_processing,
-                    'freezing_temp': biosample.freezing_temp,
-                    'thawing_time': biosample.thawing_time,
-                    'spectral_data': spectraldata.data
-                }
-                for symptom in last_visit_symptoms:
-                    if symptom.disease.value_class == "BOOL":
-                        initial_data[symptom.disease.name] = \
-                            Disease.Types(symptom.disease.value_class).cast(symptom.disease_value)   
-                    else:
-                        initial_data[symptom.disease.name] = symptom.disease_value
-                form = DataInputForm(initial=initial_data)
-                message = "The data associated with Patient ID {} is shown below:".format(patient_id)
-                return render(request, 'DataInputForm.html', {'form': form, 'message': message, \
-                    'delta_count': delta_count})
                 
     else:
         form = DataInputForm()
