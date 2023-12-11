3a26290b06773666b05d390630ba8d3ba8bfc2f9
James Noss <jnoss@jhu.edu>
2023-12-11 11:56:39 -0500

    Remove other obsolete code
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/uploader/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from uploader.models import UploadedFile
-
-
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = UploadedFile
-        fields = ["meta_data_file", "spectral_data_file"]
--- biospecdb/apps/uploader/views.py
+++ biospecdb/apps/uploader/views.py
@@ -1,33 +1,7 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
-from openpyxl import load_workbook
-
-from .forms import FileUploadForm
 
 
 @staff_member_required
 def home(request):
     return render(request, 'Home.html')
-
-
-@staff_member_required
-def upload_file(request):
-    if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            #return render(request, 'UploadSuccess.html')
-            return display_xlsx(request)
-    else:
-        form = FileUploadForm()
-    return render(request, 'MetadataFileUpload.html', {'form': form})
-
-
-@staff_member_required
-def display_xlsx(request):
-    workbook = load_workbook('./biospecdb/apps/uploader/uploads/METADATA_barauna2021ultrarapid.xlsx')
-    worksheet = workbook.active
-    data = []
-    for row in worksheet.iter_rows(values_only=True):
-        data.append(row)
-    return render(request, 'MetadataDisplay.html', {'data': data})
