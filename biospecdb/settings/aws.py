edad51c8ef6753d87bd3101ebe4fc9d4fc80685d
James Noss <jnoss@jhu.edu>
2024-03-01 11:39:08 -0500

    Store media files on S3
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/settings/aws.py
+++ biospecdb/settings/aws.py
@@ -1,12 +1,25 @@
-# import os
-
 from .prd import *  # noqa F403
+import os
 
+
+# Media files.
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "bucket_name": os.getenv("AWS_STORAGE_BUCKET_NAME"),
+            "region_name": os.getenv("AWS_S3_REGION_NAME"),
+            "use_ssl": True,
+            "url_protocol": "https"
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
--- biospecdb/settings/base.py
+++ biospecdb/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "storages",
     "nested_admin",
     "explorer",
     "uploader.apps.UploaderConfig",
